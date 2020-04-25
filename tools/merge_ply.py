ba106d3ced5e1dad60c221eefc78ad00cdc1412a
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2020-04-25 00:21:40 -0700

    Refactored merge_ply.py to merge edges and vertices, rather than just vertices

--- tools/merge_ply.py
+++ tools/merge_ply.py
@@ -2,96 +2,22 @@
 # merge_ply.py
 #
 # Recursively merges all ply files in a given directory
-# Different ply files are given different (solid) colors
+#
 
 import argparse
 import os
 
-class ParsePlyError(Exception):
-    def __init__(self, message):
-        self.message = message
+class PLYParser():
 
-def parse_ply_fp(fp, element, num_props):
-    num_prior_elements = 0
-    num_elements = 0
-    for l in fp:
-        words = l.split()
-        if words[0] == "element":
-            break
+    def __init__(self, directory):
+        # Vertices will be all the vertex lines in all the ply files
+        self.total_vertices = 0
+        # Edges will be the (derived) relevant line defining edges in all the ply files
+        self.edges = []
+        self.directory = directory
 
-    in_header = True
-    prop_count = 0
-    while in_header:
-        # At start of loop the current line (in words) is an element line
-        if words[1] == element:
-            if prop_count != 0:
-                raise ParsePlyError("Multiple element regions defined for type!")
-            num_elements = int(words[2])
-            # Iterate to next element tag, counting property tags
-            for l in fp:
-                words = l.split()
-                if words[0] == "end_header":
-                    in_header = False
-                    break
-                if words[0] == "element":
-                    break
-                if words[0] == "property":
-                    prop_count += 1
-            if prop_count != num_props:
-                raise ParsePlyError("Incorrect number of properties!")
-        else:
-            if num_elements == 0:
-                num_prior_elements += int(words[2])
-            # Iterate to next element tag
-            for l in fp:
-                words = l.split()
-                if words[0] == "end_header":
-                    in_header = False
-                    break
-                if words[0] == "element":
-                    break
-
-    if num_elements == 0:
-        # No elements found - OK
-        return
-
-    # No longer in the header region
-    for l in fp:
-        words = l.split()
-        if words[0] == "comment":
-            continue
-        if num_prior_elements > 0:
-            num_prior_elements -= 1
-            continue
-        if num_elements > 0:
-            num_elements -= 1
-            yield l
-
-
-
-def parse_ply_files(directory, element, num_props):
-    """
-    Generator parsing ply files in a directory (recursively) for the given element type
-    Arguments:
-    directory -- the directory to search through
-    element -- the element type ['vertex', 'edge']
-    num_props -- number of properties the element is expected to have.
-    A ParsePlyError exception is raised if not all ply file element definitions equal this number
-    """
-    directories = [directory]
-    while len(directories) > 0:
-        d = directories.pop()
-        with os.scandir(d) as it:
-            for dirent in it:
-                if dirent.is_dir():
-                    directories.append(dirent.path)
-                if dirent.is_file() and dirent.name[-4:] == ".ply":
-                    with open(dirent.path, 'r') as fp:
-                        yield parse_ply_fp(fp, element, num_props)
-
-def main(args):
-    directory = args.directory
-    with open(args.output, 'w') as f:
+    def merge(self, output):
+        directories = [self.directory]
         header = ["ply\n",
                   "format ascii 1.0\n",
                   "element vertex ",
@@ -103,18 +29,91 @@ def main(args):
                   "property uchar red\n",
                   "property uchar green\n",
                   "property uchar blue\n",
+                  "element edge ",
+                  # Blank space to put in the number of elements at the end
+                  "                                                     \n",
+                  "property int vertex1\n",
+                  "property int vertex2\n",
+                  "property uchar red\n",
+                  "property uchar green\n",
+                  "property uchar blue\n",
                   "end_header\n"]
-        f.writelines(header)
 
-        gen1 = parse_ply_files(directory, "vertex", 6)
-        total_elements = 0
-        for gen2 in gen1:
-            for l in gen2:
-                total_elements += 1
-                f.write(l)
+        with open(output, 'w') as out:
+            out.writelines(header)
 
-        f.seek(len(header[0]) + len(header[1]) + len(header[2]))
-        f.write("{}\ncomment space".format(total_elements))
+            while len(directories) > 0:
+                d = directories.pop()
+                with os.scandir(d) as it:
+                    for dirent in it:
+                        if dirent.is_dir():
+                            directories.append(dirent.path)
+                        if dirent.is_file() and dirent.name[-4:] == ".ply":
+                            with open(dirent.path, 'r') as fp:
+                                # Write all the vertex lines as soon as possible
+                                out.writelines(self.parse_ply_fp(fp))
+
+            print(len(self.edges))
+            self.total_edges = len(self.edges)
+            out.writelines(self.edges)
+
+            vertex_start_pos = 0
+            for h in header[:3]:
+                vertex_start_pos += len(h)
+            out.seek(vertex_start_pos)
+            out.write("{}\ncomment space".format(self.total_vertices))
+
+            edge_start_pos = 0
+            for h in header[:11]:
+                edge_start_pos += len(h)
+            out.seek(edge_start_pos)
+            out.write("{}\ncomment space".format(self.total_edges))
+
+
+    def parse_ply_fp(self, fp):
+        """
+        Yields all the vertex lines, adds the edge lines to internal datastructure
+        """
+        num_vertices = 0
+        num_edges = 0
+
+
+        in_header = True
+        while in_header:
+            words = fp.readline().split()
+            if words[0] == "end_header":
+                in_header = False
+            if words[0] != "element":
+                continue
+
+            if words[1] == "vertex":
+                num_vertices = int(words[2])
+            if words[1] == "edge":
+                num_edges = int(words[2])
+
+        if num_vertices + num_edges == 0:
+            # No elements found - OK
+            return
+
+        vertices_start = self.total_vertices
+        # No longer in the header region
+        for l in fp:
+            if num_vertices > 0:
+                yield l
+                self.total_vertices += 1
+                num_vertices -= 1 
+            elif num_edges > 0:
+                words = l.split()
+                u, v = int(words[0]), int(words[1])
+                r, g, b = words[2], words[3], words[4]
+                self.edges.append("{} {} {} {} {}\n".format(u + vertices_start, v + vertices_start, r, g, b))
+                num_edges -= 1
+            else:
+                raise Exception("Shouldn't happen")
+
+def main(args):
+    ply_parser = PLYParser(args.directory)
+    ply_parser.merge(args.output)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
